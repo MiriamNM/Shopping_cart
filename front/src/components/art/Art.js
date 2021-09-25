cb55da5f16371c728ea157269eacdc749e1105ff
Miriam NM <miriamnavamucio@gmail.com>
2021-09-25 18:52:22 -0500

    Finished Layout in home and item views

--- front/src/components/art/Art.js
+++ front/src/components/art/Art.js
@@ -6,6 +6,9 @@ import './art.css';
 const Art = (props) => {
     return(
         <div className="product" onClick={()=>(props.handleSaveData(props.item))}>
+            <div className="image-product">
+                <img src={props.item.image} alt={props.item.name}/>
+            </div>
             <h3>{props.item.name}</h3>
             <div className="container-access">
                 <img src={add_cart} alt="Add cart icon"/>
